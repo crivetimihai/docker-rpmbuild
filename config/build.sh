24d7ed90fc4dce46bdddc976efd14dc1304eddc1
Criveti Mihai <crivetimihai@gmail.com>
2019-08-05 16:04:30 -0500

    debug

--- config/build.sh
+++ config/build.sh
@@ -1,28 +1,28 @@
 #/usr/bin/env bash
 
 create_rpmtree() {
-  sudo chown -R builder: ~/rpmbuild
-  rpmdev-setuptree
+    sudo chown -R builder: ~/rpmbuild
+    rpmdev-setuptree
 }
 
 build_rpm() {
-  REPO=$1
-  SPEC=$2
+    REPO=$1
+    SPEC=$2
 
-  # Clone git repo
-  git clone $REPO ~/repo
-  cp ~/repo/*.spec ~/rpmbuild/SPECS
-  cp ~/repo/* ~/rpmbuild/SOURCES
-  rm -rf ~/repo
+    # Clone git repo
+    git clone $REPO ~/repo
+    cp ~/repo/*.spec ~/rpmbuild/SPECS
+    cp ~/repo/* ~/rpmbuild/SOURCES
+    rm -rf ~/repo
 
-  # Install deps
-  sudo yum-builddep -y ${SPEC}
+    # Install deps
+    sudo yum-builddep -y ${SPEC}
 
-  # Download sources
-  spectool -g -R ${SPEC}
+    # Download sources
+    spectool -g -R ${SPEC}
 
-  # Build
-  rpmbuild -bb ${SPEC}
+    # Build
+    rpmbuild -bb ${SPEC}
 }
 
 create_rpmtree
